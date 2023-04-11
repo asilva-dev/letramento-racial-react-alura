fb81ff1c91604af5940c35ceedfb7b3a94a50032
mandypry <amanda.adgti@gmail.com>
2023-04-10 22:22:06 -0300

    Deletando um colaborador

--- src/App.js
+++ src/App.js
@@ -217,8 +217,10 @@ function App() {
 
   const [colaboradores, setColaboradores] = useState(inicial);
 
-  function deletarColaborador() {
-    console.log('Deletando um colaborador');
+  function deletarColaborador(id) {
+    setColaboradores(
+      colaboradores.filter((colaborador) => colaborador.id !== id),
+    );
   }
 
   function mudarCorDoTime(cor, id) {
